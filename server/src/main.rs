dd125e6ca330b66f85352fa43750ebd30710841a
Erik Rimskog <erik.rimskog@gmail.com>
2022-09-07 22:06:21 +0200

    server logger

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,16 @@
+fn init_logger() {
+    use simplelog::*;
+
+    let level = LevelFilter::Debug;
+    let config = Config::default();
+    let colors = if atty::is(atty::Stream::Stdout) { ColorChoice::Auto} else { ColorChoice::Never};
+
+    TermLogger::init(level, config, TerminalMode::Stdout, colors).expect("could not init logger");
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    println!("{}", "hej");
+    init_logger();
+    log::info!("hej");
+    log::debug!("hejsan");
 }
