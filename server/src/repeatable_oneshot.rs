c8a21a04b90d0a96b7f98512e1c79327265598a1
Erik Rimskog <erik.rimskog@gmail.com>
2022-10-13 22:59:34 +0200

    fix repeat_oneshot bugs

--- server/src/repeatable_oneshot.rs
+++ server/src/repeatable_oneshot.rs
@@ -7,7 +7,7 @@ use tokio::sync::{mpsc, Mutex, Notify};
 pub struct OtherEndClosed;
 
 pub struct Sender<T> {
-    data: Arc<Mutex<Option<T>>>,
+    data: Option<Arc<Mutex<Option<T>>>>,
     notifier: Arc<Notify>,
 }
 
@@ -22,25 +22,30 @@ pub fn repeat_oneshot<T>() -> (Sender<T>, Receiver<T>) {
     let notifier = Arc::new(Notify::new());
     (
         Sender {
-            data: Arc::clone(&data),
+            data: Some(Arc::clone(&data)),
             notifier: Arc::clone(&notifier),
         },
         Receiver { data, notifier },
     )
 }
 
-impl<T> Sender<T> {
+impl<T> Sender<T>
+where
+    T: std::fmt::Debug,
+{
     pub async fn send(&self, msg: T) -> Result<(), OtherEndClosed> {
         if self.is_closed() {
             return Err(OtherEndClosed);
         }
-        self.data.lock().await.replace(msg);
+        if let Some(old) = self.data.as_ref().unwrap().lock().await.replace(msg) {
+            log::trace!("Replaced '{:?}' with new data in a rep_oneshot", old);
+        }
         self.notifier.notify_one();
         Ok(())
     }
 
     pub fn is_closed(&self) -> bool {
-        Arc::strong_count(&self.data) == 1
+        Arc::strong_count(self.data.as_ref().unwrap()) == 1
     }
 }
 
@@ -48,17 +53,22 @@ impl<T> Drop for Sender<T> {
     fn drop(&mut self) {
         // NOTE: Make sure the receiver doesn't hang if this is dropped between
         // `is_closed` and `notified`.
+        drop(self.data.take());
         self.notifier.notify_one();
     }
 }
 
 impl<T> Receiver<T> {
     pub async fn recv(&self) -> Result<T, OtherEndClosed> {
-        if self.is_closed() {
-            return Err(OtherEndClosed);
+        loop {
+            if self.is_closed() {
+                break Err(OtherEndClosed);
+            }
+            self.notifier.notified().await;
+            if let Some(x) = self.data.lock().await.take() {
+                break Ok(x);
+            }
         }
-        self.notifier.notified().await;
-        self.data.lock().await.take().ok_or(OtherEndClosed)
     }
 
     pub fn is_closed(&self) -> bool {
