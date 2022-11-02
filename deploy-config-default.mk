5cf9509e65da736a7d31a4ac9c0caf186847473d
Erik Rimskog <erik.rimskog@gmail.com>
2022-11-02 19:21:25 +0100

    rename default config

--- deploy-config-default.mk
+++ deploy-config-default.mk
@@ -5,6 +5,7 @@ DEPLOY_TARGET := # It is probably this for a linux machine: x86_64-unknown-linux
 DEPLOY_HOST := # foo@192.168.1.10
 
 # Where on the host the artifacts should go. These aren't configurable at the moment.
+# The trailing slash is important for rsync.
 DEPLOY_PATH := gcast
 DEPLOY_CLIENT_PATH := $(DEPLOY_HOST):$(DEPLOY_PATH)/client/
 DEPLOY_SERVER_PATH := $(DEPLOY_HOST):$(DEPLOY_PATH)/server/
