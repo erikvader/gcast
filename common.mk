2ddb0a340f4b1d190b92ca1c45e0a722f3a7f001
Erik Rimskog <erik.rimskog@gmail.com>
2023-03-26 21:13:24 +0200

    deploy-journal

--- common.mk
+++ common.mk
@@ -4,6 +4,7 @@ RSYNC = rsync $(RSYNCFLAGS)
 SERVICESTOP := systemctl --user stop
 SERVICESTART := systemctl --user start
 SERVICESTATUS := systemctl --user status
+SERVICEJOURNAL := journalctl --user --lines 100 --follow --unit
 
 SSH := ssh
 
