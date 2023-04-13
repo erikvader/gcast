538276f005b4fba018355546d5ede74558bba7ed
Erik Rimskog <erik.rimskog@gmail.com>
2023-04-13 21:32:00 +0200

    message_part also makes all struct fields public

--- protocol-macros/src/lib.rs
+++ protocol-macros/src/lib.rs
@@ -1,12 +1,39 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
+use syn::spanned::Spanned;
+use syn::{parse_macro_input, Fields, Item, ItemStruct, Token, Visibility};
 
 #[proc_macro_attribute]
 pub fn message_part(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let item2: proc_macro2::TokenStream = item.into();
+    let mut item = parse_macro_input!(item as Item);
+
+    match item {
+        Item::Enum(_) => (),
+        Item::Struct(ref mut item) => pubify_struct(item),
+        _ => {
+            return syn::Error::new(
+                item.span(),
+                "message_part can only be used on an enum or a struct",
+            )
+            .into_compile_error()
+            .into()
+        }
+    }
+
     let tokens = quote! {
         #[derive(Debug, PartialEq, Eq, Clone, serde::Serialize, serde::Deserialize)]
-        pub #item2
+        pub #item
     };
+
     tokens.into()
+}
+
+fn pubify_struct(struc: &mut ItemStruct) {
+    match struc.fields {
+        Fields::Named(ref mut fields) => fields.named.iter_mut().for_each(|field| {
+            field.vis = Visibility::Public(Token![pub](Span::call_site()))
+        }),
+        _ => (),
+    }
 }
