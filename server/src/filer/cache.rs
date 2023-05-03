a8ca6ff2bb4c8c8768216dab314ccf73158c5cac
Erik Rimskog <erik.rimskog@gmail.com>
2023-05-03 23:40:10 +0200

    story directory hierarchy in the cache

--- server/src/filer/cache.rs
+++ server/src/filer/cache.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::{create_dir_all, File},
     path::{Path, PathBuf},
     time::SystemTime,
@@ -20,15 +21,37 @@ const EXT_WHITELIST: &[&str] = &[".mp4", ".mkv", ".wmv", ".webm", ".avi"];
 #[derive(Debug, serde::Serialize, serde::Deserialize, Default)]
 pub struct Cache {
     files: Vec<CacheEntry>,
+    dirs: Vec<CacheDirEntry>,
+    root_dir: Vec<Pointer>,
     updated: Option<SystemTime>,
     roots: Vec<String>,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub(super) struct CacheEntry {
+    // TODO: store as PathBuf instead, and do to_string_lossy when sending to the client.
+    // Or maybe store the path_relative_root as a String as well?
+    // Or will this just ruin mpv play? I think it requires the paths to be rust strings
+    // TODO: Varför sparar jag ens hela sökvägen? Räcker inte path relative root? Då kan
+    // root_len försvinna. Det finns väl ingenting som vill ha hela sökvägen? Det borde gå
+    // att slå ihop med Cache::roots annars.
     path: String,
     root: usize,
+    // TODO: root_len could be number of segments in the path intead of the number of
+    // bytes
     root_len: usize,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub(super) struct CacheDirEntry {
+    entry: CacheEntry,
+    children: Vec<Pointer>,
+}
+
+#[derive(Debug, Clone, Copy, serde::Serialize, serde::Deserialize)]
+pub enum Pointer {
+    File(usize),
+    Dir(usize),
 }
 
 impl CacheEntry {
@@ -48,6 +71,8 @@ impl CacheEntry {
         &self.path[self.root_len..]
     }
 
+    /// The character index in `path_relative_root` where the basename starts, i.e., the
+    /// last path separator.
     pub(super) fn basename_char(&self) -> usize {
         match self
             .path_relative_root()
@@ -62,6 +87,35 @@ impl CacheEntry {
     }
 }
 
+impl CacheDirEntry {
+    fn new(path: String, root: usize, root_len: usize) -> Self {
+        Self {
+            entry: CacheEntry::new(path, root, root_len),
+            children: vec![],
+        }
+    }
+
+    fn set_children(&mut self, children: Vec<Pointer>) {
+        self.children = children;
+    }
+
+    delegate::delegate! {
+        to self.entry {
+            pub(super) fn root(&self) -> usize;
+            pub(super) fn path_relative_root(&self) -> &str;
+            pub(super) fn basename_char(&self) -> usize;
+        }
+    }
+
+    pub(super) fn children(&self) -> &[Pointer] {
+        &self.children
+    }
+
+    pub(super) fn cache_entry(&self) -> &CacheEntry {
+        &self.entry
+    }
+}
+
 impl AsRef<str> for CacheEntry {
     fn as_ref(&self) -> &str {
         self.path_relative_root()
@@ -69,11 +123,18 @@ impl AsRef<str> for CacheEntry {
 }
 
 impl Cache {
-    fn new(files: Vec<CacheEntry>, roots: Vec<String>) -> Self {
+    fn new(
+        files: Vec<CacheEntry>,
+        dirs: Vec<CacheDirEntry>,
+        roots: Vec<String>,
+        root_dir: Vec<Pointer>,
+    ) -> Self {
         Self {
             files,
+            dirs,
             updated: Some(SystemTime::now()),
             roots,
+            root_dir,
         }
     }
 
@@ -87,6 +148,21 @@ impl Cache {
 
     pub fn is_outdated(&self, roots: &[String]) -> bool {
         self.roots != roots
+    }
+
+    pub(super) fn deref(&self, pointer: Pointer) -> &CacheEntry {
+        match pointer {
+            Pointer::Dir(i) => self
+                .dirs
+                .get(i)
+                .expect("a pointer is always valid")
+                .cache_entry(),
+            Pointer::File(i) => self.files.get(i).expect("a pointer is always valid"),
+        }
+    }
+
+    pub(super) fn deref_dir_raw(&self, pointer: usize) -> Option<&CacheDirEntry> {
+        self.dirs.get(pointer)
     }
 }
 
@@ -143,7 +219,7 @@ where
     let shallow = shallow_scan(&roots, &mut prog_report, &mut root_status).await?;
 
     log::info!("Doing a deep scan of all roots...");
-    let files = deep_scan(
+    let deep = deep_scan(
         &shallow.dirs,
         &mut prog_report,
         &roots,
@@ -153,13 +229,14 @@ where
     .await?;
 
     log::info!("Creating a cache from all files...");
+    let shallow_dirs_len = shallow.dirs.len();
     let cache = create_cache_from_files(
-        files,
+        deep.files.into_iter().chain(shallow.files),
+        deep.dirs.into_iter().chain(shallow.dirs),
         roots,
         num_errors,
         &mut prog_report,
-        shallow.files,
-        shallow.dirs.len(),
+        shallow_dirs_len,
         &root_status,
     )
     .await?;
@@ -206,11 +283,11 @@ where
 }
 
 async fn create_cache_from_files<F, Fut>(
-    files: Vec<(usize, DirEntry)>,
+    files: impl Iterator<Item = (usize, DirEntry)>,
+    dirs: impl Iterator<Item = (usize, DirEntry)>,
     roots: Vec<String>,
     mut num_errors: usize,
     mut prog_report: F,
-    files_shallow: Vec<(usize, DirEntry)>,
     num_dirs: usize,
     root_status: &[filesearch::refreshing::RootStatus],
 ) -> Result<Cache, FilerError>
@@ -218,9 +295,7 @@ where
     F: FnMut(filesearch::refreshing::Refreshing) -> Fut,
     Fut: Future<Output = ()>,
 {
-    let mut cache_files: Vec<CacheEntry> = files_shallow
-        .into_iter()
-        .chain(files)
+    let mut cache_files: Vec<CacheEntry> = files
         .filter_map(|(i, de)| match create_cache_entry(de, i, &roots) {
             Ok(None) => None,
             Ok(Some(ce)) => Some(ce),
@@ -230,6 +305,34 @@ where
             }
         })
         .collect();
+
+    let mut cache_dirs: Vec<CacheDirEntry> = dirs
+        .filter_map(|(i, de)| match create_cache_dir_entry(de, i, &roots) {
+            Ok(ce) => Some(ce),
+            Err(()) => {
+                num_errors += 1;
+                None
+            }
+        })
+        .collect();
+    cache_dirs.extend(surface_scan(&roots));
+
+    cache_files
+        .sort_unstable_by(|e1, e2| e1.path_relative_root().cmp(e2.path_relative_root()));
+
+    cache_dirs
+        .sort_unstable_by(|e1, e2| e1.path_relative_root().cmp(e2.path_relative_root()));
+
+    let (children, root_indices) = link(&cache_files, &cache_dirs);
+    children.into_iter().for_each(|(i, pointers)| {
+        cache_dirs
+            .get_mut(i)
+            .expect("the indices came from this vec")
+            .set_children(pointers)
+    });
+
+    let root_dir_pointers: Vec<Pointer> =
+        root_indices.into_iter().map(|i| Pointer::Dir(i)).collect();
 
     prog_report(make_refreshing(
         num_dirs,
@@ -241,9 +344,59 @@ where
     ))
     .await;
 
-    cache_files
-        .sort_unstable_by(|e1, e2| e1.path_relative_root().cmp(e2.path_relative_root()));
-    Ok(Cache::new(cache_files, roots))
+    Ok(Cache::new(
+        cache_files,
+        cache_dirs,
+        roots,
+        root_dir_pointers,
+    ))
+}
+
+fn link(
+    files: &[CacheEntry],
+    dirs: &[CacheDirEntry],
+) -> (HashMap<usize, Vec<Pointer>>, Vec<usize>) {
+    let mut roots = Vec::new();
+    let dirs_inverted: HashMap<&str, usize> = dirs
+        .iter()
+        .enumerate()
+        .map(|(i, entry)| (entry.entry.path.as_str(), i))
+        .collect();
+    let mut children: HashMap<&str, Vec<Pointer>> = dirs
+        .iter()
+        .map(|entry| (entry.entry.path.as_str(), vec![]))
+        .collect();
+
+    for (i, d) in dirs.iter().enumerate() {
+        match dirname(&d.entry.path).and_then(|parent| children.get_mut(parent)) {
+            Some(pointers) => pointers.push(Pointer::Dir(i)),
+            None => roots.push(i),
+        }
+    }
+
+    for (i, f) in files.iter().enumerate() {
+        match dirname(&f.path).and_then(|parent| children.get_mut(parent)) {
+            Some(pointers) => pointers.push(Pointer::File(i)),
+            None => log::error!(
+                "The file '{:?}' does not have a parent directory for some reason",
+                f
+            ),
+        }
+    }
+
+    let children: HashMap<usize, Vec<Pointer>> = children
+        .into_iter()
+        .map(|(path, pointers)| {
+            (
+                *dirs_inverted
+                    .get(path)
+                    .expect("both maps have the same keys"),
+                pointers,
+            )
+        })
+        .collect();
+
+    (children, roots)
 }
 
 fn explode(
@@ -284,21 +437,48 @@ fn create_cache_entry(
     }
 }
 
+fn create_cache_dir_entry(
+    de: DirEntry,
+    i: usize,
+    roots: &[String],
+) -> Result<CacheDirEntry, ()> {
+    match de.into_path().into_os_string().into_string() {
+        Ok(path) => Ok(CacheDirEntry::new(
+            path,
+            i,
+            roots
+                .get(i)
+                .expect("i is from enumerate, i.e. always in range")
+                .len(),
+        )),
+        Err(path) => {
+            log::error!("Failed to convert '{:?} to a String", path);
+            Err(())
+        }
+    }
+}
+
+struct Scan {
+    files: Vec<(usize, DirEntry)>,
+    dirs: Vec<(usize, DirEntry)>,
+}
+
 async fn deep_scan<F, Fut>(
-    dirs: &[(usize, DirEntry)],
+    scan_in: &[(usize, DirEntry)],
     mut prog_report: F,
     roots: &[String],
     root_status: &[filesearch::refreshing::RootStatus],
     num_errors: &mut usize,
-) -> Result<Vec<(usize, DirEntry)>, FilerError>
+) -> Result<Scan, FilerError>
 where
     F: FnMut(filesearch::refreshing::Refreshing) -> Fut,
     Fut: Future<Output = ()>,
 {
     let mut files: Vec<(usize, DirEntry)> = Vec::new();
-    let total_dirs = dirs.len();
+    let mut dirs: Vec<(usize, DirEntry)> = Vec::new();
+    let total_dirs = scan_in.len();
 
-    for (i, (root, dir)) in dirs.iter().enumerate() {
+    for (i, (root, dir)) in scan_in.iter().enumerate() {
         prog_report(make_refreshing(
             i,
             total_dirs,
@@ -309,30 +489,26 @@ where
         ))
         .await;
 
-        for de in WalkDir::new(dir.path()) {
+        for de in WalkDir::new(dir.path()).min_depth(1) {
             match de {
                 Err(e) => {
                     log::error!("Failed to walk: {}", e);
                     *num_errors += 1;
                 }
                 Ok(e) if e.file_type().is_file() => files.push((*root, e)),
+                Ok(e) if e.file_type().is_dir() => dirs.push((*root, e)),
                 Ok(_) => (),
             }
         }
     }
-    Ok(files)
-}
-
-struct ShallowScan {
-    files: Vec<(usize, DirEntry)>,
-    dirs: Vec<(usize, DirEntry)>,
+    Ok(Scan { files, dirs })
 }
 
 async fn shallow_scan<F, Fut>(
     roots: &[String],
     mut prog_report: F,
     root_status: &mut [filesearch::refreshing::RootStatus],
-) -> Result<ShallowScan, FilerError>
+) -> Result<Scan, FilerError>
 where
     F: FnMut(filesearch::refreshing::Refreshing) -> Fut,
     Fut: Future<Output = ()>,
@@ -379,7 +555,15 @@ where
     ))
     .await;
 
-    Ok(ShallowScan { files, dirs })
+    Ok(Scan { files, dirs })
+}
+
+fn surface_scan(roots: &[String]) -> Vec<CacheDirEntry> {
+    roots
+        .iter()
+        .enumerate()
+        .map(|(i, path)| CacheDirEntry::new(path.clone(), i, path.len()))
+        .collect()
 }
 
 fn has_whitelisted_extension(path: &str) -> bool {
@@ -410,4 +594,16 @@ fn make_refreshing(
         is_done,
     };
     msg
+}
+
+fn basename(path: &str) -> Option<&str> {
+    Path::new(path)
+        .file_name()
+        .map(|osstr| osstr.to_str().expect("this is a subset of a rust string"))
+}
+
+fn dirname(path: &str) -> Option<&str> {
+    Path::new(path)
+        .parent()
+        .map(|osstr| osstr.to_str().expect("this is a subset of a rust string"))
 }
