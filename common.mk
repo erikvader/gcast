15255a9e068d3686e612de77185918ff28d1d844
Erik Rimskog <erik.rimskog@gmail.com>
2024-06-24 00:21:35 +0200

    make deploy release as default

--- common.mk
+++ common.mk
@@ -1,4 +1,4 @@
-RSYNCFLAGS := -avhs --delete
+RSYNCFLAGS = -avhs --delete
 RSYNC = rsync $(RSYNCFLAGS)
 
 SERVICE_STOP := systemctl --user stop
@@ -6,17 +6,17 @@ SERVICE_START := systemctl --user start
 SERVICE_STATUS := systemctl --user status
 SERVICE_JOURNAL := journalctl --user --lines 100 --follow --output short-precise --unit
 
-SSHFLAGS := -t
+SSHFLAGS = -t
 SSH = ssh $(SSHFLAGS)
 
-CARGOFLAGS :=
+CARGOFLAGS =
 CARGO = cargo $(CARGOFLAGS)
 
-CARGO_BUILDFLAGS :=
+CARGO_BUILDFLAGS =
 CARGO_BUILD = $(CARGO) build $(CARGO_BUILDFLAGS)
 
-CARGO_RUNFLAGS :=
+CARGO_RUNFLAGS =
 CARGO_RUN = $(CARGO) run $(CARGO_RUNFLAGS)
 
-CURLFLAGS := -L
+CURLFLAGS = -L
 CURL = curl $(CURLFLAGS)
